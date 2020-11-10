7019cf3bd7ac664268b4d2c5863e6b99669b3fa1
kimseokho582 <gjrehf@naver.com>
2020-11-10 21:11:10 +0900

    solar system

--- ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -1,32 +1,71 @@
-﻿#include <gl/glut.h>					
-#include <gl/gl.h>						
-#include <gl/glu.h>	
-
+﻿#include <gl/glut.h>
+#include <gl/gl.h>
+#include <gl/glu.h>
+static int Day = 0, Time = 0;
 void MyDisplay() {
     glClear(GL_COLOR_BUFFER_BIT);
-    glViewport(0, 0, 300, 300);
-
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
-    for (int i = 1; i <= 4; i++) {
-        for (int j = 0; j < 3; j++) {
-            glPushMatrix();
-            glColor3f(0.3 * j, 0.1 * i, 0.0);
-            glTranslatef(0.1 * j, 0.1 * i, 0);
-            glutSolidCube(0.1);
-            glPopMatrix();
-        }
-    }
+    glColor3f(1.0, 0.0, 0.0);
+    glutWireSphere(0.2, 20, 16);  //Sun
 
-    glFlush();
+    glPushMatrix();
+    glRotatef((GLfloat)Day, 0.0, 0.0, 1.0);
+    glTranslatef(0.7, 0.0, 0.0);  /////////////////						
+    glRotatef((GLfloat)Time, 0.0, 0.0, 1.0);
+    glColor3f(0.0, 1.0, 0.0);
+    glutWireSphere(0.1, 10, 8);// Earth
+
+    glPushMatrix();
+    glRotatef((GLfloat)Time, 0.0, 0.0, 1.0);
+    glTranslatef(0.2, 0.0, 0.0); /////////////////				
+    glColor3f(0.0, 0.0, 1.0);
+    glutWireSphere(0.04, 10, 8);  // Moon
+    glBegin(GL_LINES);
+    glVertex3f(0.0, 0.0, 0.0);
+    glVertex3f(0.5, 0.0, 0.0);
+    glEnd();
+
+    glPopMatrix();
+
+    //glColor3f(1.0, 0.0, 1.0);    //CTM
+   // glutWireSphere(0.05, 20, 16);  // ?????
+
+    glBegin(GL_LINES);
+    glColor3f(0.0, 1.0, 0.0);
+    glVertex3f(0.0, 0.0, 0.0);
+    glVertex3f(0.5, 0.0, 0.0);
+    glEnd();
+
+    glPopMatrix();
+
+    //glColor3f (0.0, 1.0, 1.0);     //CTM
+    //glutWireSphere(0.02, 20, 16);
+
+    glutSwapBuffers();
+}
+
+void MyKeyboard(unsigned char key, int x, int y) {
+    switch (key) {
+    case 'd':
+        Day = (Day + 90) % 360;
+        glutPostRedisplay();
+        break;
+    case 't':
+        Time = (Time + 45) % 360;
+        glutPostRedisplay();
+        break;
+    default:
+        break;
+    }
 }
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_RGBA);
-    glutInitWindowSize(300, 300);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(500, 500);
     glutInitWindowPosition(0, 0);
     glutCreateWindow("OpenGL Sample Drawing");
     glClearColor(1.0, 1.0, 1.0, 1.0);
@@ -36,6 +75,7 @@ int main(int argc, char** argv) {
     glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
 
     glutDisplayFunc(MyDisplay);
+    glutKeyboardFunc(MyKeyboard);
 
     glutMainLoop();
     return 0;
